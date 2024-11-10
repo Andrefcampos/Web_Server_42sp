b4318a4851192d1d8e3b8ec42ab4449a4dbf0740
Andrefcampos <andrecampos.wnr@gmail.com>
2024-11-10 19:31:49 -0300

    Upload: html pages, style.css and scripts.js

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: andrefil <andrefil@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 11:41:42 by rbutzke           #+#    #+#             */
-/*   Updated: 2024/11/08 03:06:38 by andrefil         ###   ########.fr       */
+/*   Updated: 2024/11/08 03:21:28 by andrefil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 int main(){
 	std::map<std::string, Server> Services;
 	Server	web1(8080, 5, "localhost:8080", "0.0.0.0");
-	web1.setPathIndex("index/index.html");
+	web1.setPathIndex("html/");
 	web1.setPathImage("image/images.png");
 	Server	web2(8081, 5, "localhost:8081", "0.0.0.0");
 	web2.setPathIndex("index/index2.html");
