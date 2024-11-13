0813bba61647921383368439cb950ce2459e91ef
Rafael <leafarbutzke@hotmail.com>
2024-11-13 18:18:36 -0300

    ...

---
+++ src/Parser/Directive.hpp
@@ -0,0 +1,98 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Directive.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rbutzke <rbutzke@student.42sp.org.br>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/12 18:29:20 by myokogaw          #+#    #+#             */
+/*   Updated: 2024/11/13 17:32:16 by rbutzke          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+# include <map>
+# include <string>
+class Server;
+
+class Directive {
+	public:
+		virtual ~Directive() {};
+};
+
+class ServerDirective : public Directive {
+	private:
+		std::map<std::string, Server *> _servers;
+	public:
+		ServerDirective();
+		~ServerDirective();
+		void			appendServer(const Server &server);
+		Server			*back(void);
+		const Server	*getServer(const std::string &host);
+};
+
+// class ListenDirective : public Directive {
+// 	private:
+// 		const std::string _value;
+// 	public:
+// 		ListenDirective(const std::string &value);
+// 		~ListenDirective() {};
+// };
+
+// class ServerNameDirective : public Directive {
+// 	public:
+// 		~ServerNameDirective() {};
+// };
+
+// class ClientMaxBodySizeDirective : public Directive {
+// 	public:
+// 		~ClientMaxBodySizeDirective() {};
+// };
+
+// class LocationDirective : public Directive {
+// 	public:
+// 		~LocationDirective() {};
+// };
+
+// class AllowMethodsDirective : public Directive {
+// 	public:
+// 		~AllowMethodsDirective() {};
+// };
+
+// class RedirectDirective : public Directive {
+// 	public:
+// 		~RedirectDirective() {};
+// };
+
+// class RootDirective : public Directive {
+// 	public:
+// 		~RootDirective() {};
+// };
+
+// class AutoIndexDirective : public Directive {
+// 	public:
+// 		~AutoIndexDirective() {};
+// };
+
+// class IndexDirective : public Directive {
+// 	public:
+// 		~IndexDirective() {};
+// };
+
+// class CgiDirective : public Directive {
+// 	public:
+// 		~CgiDirective() {};
+// };
+
+// class UploadDirDirective : public Directive {
+// 	public:
+// 		~UploadDirDirective() {};
+// };
+
+// class ErrorPageDirective : public Directive {
+// 	public:
+// 		~ErrorPageDirective() {};
+// };
+
+// void	initConf(void);
