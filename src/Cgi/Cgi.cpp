52274d878aa1451f1807e5793d14747c1a4670cb
Andrefcampos <andrecampos.wnr@gmail.com>
2024-11-26 11:00:31 -0300

    Edit: cgi class

--- src/Cgi/Cgi.cpp
+++ src/Cgi/Cgi.cpp
@@ -6,7 +6,7 @@
 /*   By: andrefil <andrefil@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/23 17:09:53 by andrefil          #+#    #+#             */
-/*   Updated: 2024/11/23 17:18:51 by andrefil         ###   ########.fr       */
+/*   Updated: 2024/11/26 11:00:07 by andrefil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,21 @@ Cgi::Cgi(Cgi const &obj) {
 	*this = obj;
 }
 
-Cgi	&Cgi::operator=(Cgi const &obj) {
-	if (this == &obj) {
-		_path = obj._path;
-		_extensions = obj._extensions;
-	}
-	return (*this);
-}
+// Cgi	&Cgi::operator=(Cgi const &obj) {
+// 	if (this == &obj) {
+// 		_path = obj._path;
+// 		_extensions = obj._extensions;
+// 	}
+// 	return (*this);
+// }
 
 Cgi::~Cgi(void) {}
 
-Cgi::Cgi(std::string const &Path, std::map<std::string, std::string> const &Extensions) : _path(Path), _extensions(Extensions){
+Cgi::Cgi(std::string const &ScriptPath, std::map<std::string, std::string> const &Envp, std::string const &Body) : \
+	_scriptpath(ScriptPath), _envp(Envp), _request(Body) {}
 
+std::string Cgi::executeCgi(void) {
+
+	//config Envp:
+	
 }
--- src/Cgi/Cgi.hpp
+++ src/Cgi/Cgi.hpp
@@ -6,7 +6,7 @@
 /*   By: andrefil <andrefil@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/23 16:15:12 by andrefil          #+#    #+#             */
-/*   Updated: 2024/11/23 17:18:17 by andrefil         ###   ########.fr       */
+/*   Updated: 2024/11/26 09:36:05 by andrefil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,20 @@
 
 class	Cgi {
 	private:
-		std::string _path;
-		std::map<std::string, std::string> _extensions;
+		std::string _scriptpath;
+		std::map<std::string, std::string> _envp;
+		std::string _request;
 	
 		Cgi(void);
 		Cgi(Cgi const &obj);
 		Cgi &operator=(Cgi const &obj);
 
 	public:
-		Cgi(std::string const &Path, std::map<std::string, std::string> const &Extensions);
+		Cgi(std::string const &ScriptPath, std::map<std::string, std::string> const &Envp, std::string const &Body);
 		~Cgi(void);
 
 		const std::string &getPath(void) const;
 		const std::string &getExtensions(void) const;
 
-		void	executeCgi(void);
+		std::string	executeCgi(void);
 };
