11b08608ded7448663b31865fb64c65c435e720c
Marcal <marcal.yokogawa@gmail.com>
2024-12-10 23:10:03 -0300

    add: namespace std, implementation of getRoute, setRoute and print functions

--- src/Server/Location/Location.cpp
+++ src/Server/Location/Location.cpp
@@ -6,12 +6,14 @@
 /*   By: myokogaw <myokogaw@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/29 02:19:28 by myokogaw          #+#    #+#             */
-/*   Updated: 2024/12/10 16:55:13 by myokogaw         ###   ########.fr       */
+/*   Updated: 2024/12/10 23:08:12 by myokogaw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Location.hpp"
 #include "Directive.hpp"
+
+using namespace std;
 
 Location::Location() : _route("") {
     this->_directives["allow_methods"] = new AllowMethodsDirective();
@@ -21,15 +23,29 @@ Location::Location() : _route("") {
 }
 
 Location::~Location() {
-    for (std::map<std::string, Directive *>::iterator it = _directives.begin(); it != _directives.end(); ++it)
+    for (map<string, Directive *>::iterator it = _directives.begin(); it != _directives.end(); ++it)
         delete it->second;
     _directives.clear();
 }
 
-void    Location::setDirective(const Directive *directive) {
-    this->_directives[directive->getName()] = const_cast<Directive *>(directive);
+void    Location::setDirective(Directive *directive) {
+    this->_directives[directive->getName()] = directive;
 }
 
-const Directive *Location::getDirective(const std::string &directive) {
+Directive *Location::getDirective(const string &directive) {
     return (this->_directives[directive]);
+}
+
+const string &Location::getRoute(void) const {
+    return (this->_route);
+}
+
+void        Location::setRoute(const string &route) {
+    this->_route = route;
+}
+
+void    Location::print(void) const {
+    for (map<string, Directive *>::const_iterator it = _directives.begin(); it != _directives.end(); ++it) {
+        it->second->print();
+    }
 }
