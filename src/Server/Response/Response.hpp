5cca2bf4bfc2a103f55afe50e6cb43c7dcf8b707
Marcal <marcal.yokogawa@gmail.com>
2024-12-17 15:08:44 -0300

    redefining class Response

--- src/Server/Response/Response.hpp
+++ src/Server/Response/Response.hpp
@@ -3,84 +3,80 @@
 /*                                                        :::      ::::::::   */
 /*   Response.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rbutzke <rbutzke@student.42sp.org.br>      +#+  +:+       +#+        */
+/*   By: myokogaw <myokogaw@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 16:07:34 by rbutzke           #+#    #+#             */
-/*   Updated: 2024/12/06 18:35:59 by rbutzke          ###   ########.fr       */
+/*   Updated: 2024/12/17 14:57:30 by myokogaw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
-#include <string>
-#include <sstream>
-#include <fstream>
-#include <sys/socket.h>
+# include <string>
+# include <sstream>
+# include <fstream>
+# include <exception>
+# include <map>
+# include <vector>
+# include <sys/socket.h>
+
+# define CRLF "\r\n"
+# define LF "\n"
+# define SP " "
+
+// RFC 9112 6.1
+// A server that receives a request message with a transfer coding it does not understand SHOULD respond with 501 (Not Implemented).
+
+// A server MAY reject a request that contains a message body but not a Content-Length by responding with 411 (Length Required).
+
+// A server that does not support persistent connections MUST send the "close" connection option
+// in every response message that does not have a 1xx (Informational) status code.
+
+class Request;
+class Location;
+class LocationDirective;
+class ErrorPageDirective;
+
+struct FieldLine {
+	std::string field_name;
+	std::string field_value;
+	FieldLine(const std::string &field_name, const std::string &field_value);
+	const std::string getFieldLine(void) const;
+};
+
+struct MimeTypes {
+	std::map<std::string, std::string> mime_types;
+	MimeTypes();
+	const std::string getMimeType(const std::string &extension);
+};
 
 class Response {
+	// attributes
 	private:
-		std::string	_fillHttp[4][4];
-		std::string	_http;
-		std::string	_status[4];
-		std::string	_type[3];
-		std::string	_connection[3];
-		std::string	_length[3];
-		std::string	_body;
-		void 		clean();
-	
-	protected:
-		std::string _pathIndex;
-		std::string _pathImage;
+		LocationDirective			*_location;
+		ErrorPageDirective			*_error_page;
+		const std::string			_http_version;
+		Location					*_cur_loc;
+		std::string					_code;
+		std::string					_reason_phrase;
+		std::vector<FieldLine *>	_header_fields;
+		std::string					_body;
+		size_t						_msg_size;
 
+	// private methods
+	private:
+		void				validationResponse(Request *request);
+		void				setStatusLine(const int code);
+		void				getError(void);
+		const std::string	getStatusLine(void) const;
+		std::string			resolveURI(const std::string &uri);
+		void				getBody(const std::string &uri);
+
+	// public methods
 	public:
-		Response();
+		Response(Request *request, LocationDirective *location, ErrorPageDirective *error_page);
 		~Response();
-		void setStatus(std::string value, std::string reason);
-		void setType(std::string type);
-		void setConnection(std::string connection);
-		void setLength(std::string length);
-		void setBody(std::string body);
-		std::string	getHttp();
-		//virtual void	sendResponse(int fd, httpRequest http) = 0;
-		void		sendIndex(int fd, std::string indexHTML);
-		void		sendImage(int fd, std::string image);
-		std::string getPathImage() const;
-		std::string getPathIndex() const;
-		void		setPathImage(std::string pathImage);
-		void		setPathIndex(std::string pathIndex);
-};
 
-class Get : public Response {
-	
-};
-
-class Post : public Response {
-	
-};
-
-class Delete : public Response {
-	
-};
-
-enum e_response
-{
-	STATUS = 0,
-	TYPE,
-	CONNECTION,
-	LENGTH,
-};
-
-enum e_status
-{
-	HTTP_VERSION = 0,
-	VALUE,
-	REASON,
-	END_LINE,
-};
-
-enum e_header
-{
-	HEADER = 0,
-	HEADER_TYPE,
-	ENDL,
+		const std::string	getResponseMsg(void);
+		size_t				getMsgSize(void) const;
 };
